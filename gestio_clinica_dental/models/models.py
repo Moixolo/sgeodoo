6ac22f34d124b68afae778deed94e6a8c007178d
moixolo <moisesasix@gmail.com>
2021-12-20 02:43:58 +0000

    tots els fields fets, les vistes basiques dels tree i les relacions corresponnents amb soles una relacio fallida

--- gestio_clinica_dental/models/models.py
+++ gestio_clinica_dental/models/models.py
@@ -2,15 +2,17 @@
 
 from datetime import time
 from re import match
+from xmlrpc.client import DateTime
 from odoo import models, fields, api
 
 # *************************************************************************************   
 # PEOPLE DATES
 # *************************************************************************************  
 
-class Pacient(models.Model):
-    _name = 'gestio_clinica_dental.Pacient'
-    _description = 'All information from pacient'
+
+class patient(models.Model):
+    _name = 'gestio_clinica_dental.patient'
+    _description = 'All information from patient'
 
     #personal dates
     name = fields.Char()
@@ -22,48 +24,48 @@ class Pacient(models.Model):
 
     #medical dates
     medicalid = fields.Integer()
-    pacientshortdescription = fields.Text()
-    salutformulary = fields.Many2many(comodel_name='gestio_clinica_dental.Salutformulary', relation='formclinic')#restric to one
-    #anamnesis = fields.Many2many()#restric to one
-    medicalstory = fields.Many2many(comodel_name='gestio_clinica_dental.Medicalhistory', relation='medicalhistoryonepatient')#restric to one
-    doctorpac = fields.Many2many(comodel_name='gestio_clinica_dental.Doctor', relation='doctorpatientassigned')
+    patientshortdescription = fields.Text()
+    salutformulary = fields.Many2many(comodel_name='gestio_clinica_dental.salutformulary', relation='formclinic')#restric to one
+    #anamnesispat = fields.Many2many(comodel_name="gestio_clinica_dental.anamnesis",relation='anampatient')#restric to one
+    medicalstory = fields.Many2many(comodel_name='gestio_clinica_dental.medicalhistory', relation='medicalhistoryonepatient')#restric to one
+    doctorpac = fields.Many2many(comodel_name='gestio_clinica_dental.doctor', relation='doctorpatientassigned')
 
     #economicdates
-    budgetpac = fields.Many2one(comodel_name='gestio_clinica_dental.Pacient')
-    paysstatus = fields.Selection([("1","No pay", ("2","Parcial pay"), ("3","Finalized"))])
+    patientbud = fields.Many2one(comodel_name='gestio_clinica_dental.budget')
+    paysstatus = fields.Selection([("1","No pay"), ("2","Parcial pay"), ("3","Finalized")])
 
     #schedule dates
-    quotes = fields.Many2one(comodel_name='gestio_clinica_dental.Quotes', relation='quotesagendfrompatient')
-    status = fields.Selection([("1","Advised", ("2","Inside cabinet"), ("3","Finalized"), ("4","Other"))])
+    quotes = fields.Many2one(comodel_name='gestio_clinica_dental.quotes', relation='quotesagendfrompatient')
+    status = fields.Selection([("1","Advised"), ("2","Inside cabinet"), ("3","Finalized"), ("4","Other")])
 
     #others
     afterimg = fields.Image()
     beforeimg = fields.Image()
 
 
-class Doctor (models.Model):
-    _name = 'gestio_clinica_dental.Doctor'
-    _description = 'Doctor description'
+class doctor (models.Model):
+    _name = 'gestio_clinica_dental.doctor'
+    _description = 'doctor description'
     #personal dates
     name = fields.Char()
     surname1 = fields.Char()
     surname2 = fields.Char()
     phonenumber = fields.Char()
     direction = fields.Text()
-
+    
+    
     #profesional dates
     collegiatenumber = fields.Char()
     specialitydoc = fields.Selection([('1','General'),('2','Surgery'),('3','Orthodontics'),('4','Endodontics'),('5','Stetic'),('6','Prosthodontist'),('7','Pediatric')])
-    
-    #items dates
-    pacientsintervened = fields.Many2many(comodel_name='gestio_clinica_dental.Patient', relation='doctorpatientassigned')
-    quotesdoc = fields.Many2many(comodel_name='gestio_clinica_dental.Quotes', relation='doctorfrompatient')
-    doctorscheduledoc = fields.Many2many(comodel_name='gestio_clinica_dental.Doctor', relation='doctorschedule')
+    #forenkeys
+    #items dates 
+    patientsintervened = fields.Many2many(comodel_name='gestio_clinica_dental.patient', relation='doctorpatientassigned')
+    doctorsch = fields.Many2many(comodel_name='gestio_clinica_dental.principalschedule', relation='doctorschedule')
     
 
 
-class Medicalassistant (models.Model):
-    _name = 'gestio_clinica_dental.Medicalassistant'
+class medicalassistant (models.Model):
+    _name = 'gestio_clinica_dental.medicalassistant'
     _description = 'Medical assistant description'
 
     #personal dates
@@ -75,8 +77,8 @@ class Medicalassistant (models.Model):
 
     #profesional dates
     collegiatenumber = fields.Char()
-
-    assistantbud= fields.Many2many(comodel_name='gestio_clinica_dental.Budget', relation='assistatintervention')#assistant
+    #items dates
+    assistantbud= fields.Many2many(comodel_name='gestio_clinica_dental.budget', relation='assistatintervention')#assistant
 
 
 
@@ -84,10 +86,10 @@ class Medicalassistant (models.Model):
 # MEDICAL DOCUMENTATION DATES
 # *************************************************************************************   
  
-class Salutformulary(models.Model):
-    _name = 'gestio_clinica_dental.Salutformulary'
-    _description = 'The pacient write this obligatorily'
-
+class salutformulary(models.Model):
+    _name = 'gestio_clinica_dental.salutformulary'
+    _description = 'The patient write this obligatorily'
+    #basic dates
     idform = fields.Integer()
     alergiesbool = fields.Boolean()                 #obligatory
     alergies = fields.Char()                        #obligatory 
@@ -95,83 +97,101 @@ class Salutformulary(models.Model):
     diseases = fields.Char()                        #obligatory
     afterdentalproblemsbool = fields.Boolean()
     afterdentalproblems = fields.Char()
+    #forenkeys
+    patientssal = fields.Many2many(comodel_name='gestio_clinica_dental.patient', relation='formclinic')#restric to one
 
-    pacientssal = fields.Many2many(comodel_name='gestio_clinica_dental.Patient', relation='formclinic')#restric to one
-
-class Consent(models.Model):
-    _name = 'gestio_clinica_dental.Consent'
-    _description = 'The pacient write this obligatorily and is unic for treatment'
-
+class consent(models.Model):
+    _name = 'gestio_clinica_dental.consent'
+    _description = 'The patient write this obligatorily and is unic for treatment'
+    #basic dates
     idform = fields.Integer()
     title = fields.Char()                 #obligatory
     text = fields.Text()                  #obligatory 
     youconsent = fields.Boolean()         #obligatory
+    #forenkeys
+    treatmentcon = fields.Many2many(comodel_name='gestio_clinica_dental.treatment', relation='consentsd')     #restric to one
 
-    treatmentcon = fields.Many2many(comodel_name='gestio_clinica_dental.Treatment', relation='consentsd')     #restric to one
-    
+class anamnesis(models.Model):
+    _name = 'gestio_clinica_dental.anamnesis'
+    _description = 'All information from the patient'
+    #basic dates
+    idform = fields.Integer()
+    alergiesbool = fields.Boolean()                 #obligatory
+    alergies = fields.Char()                        #obligatory 
+    diseasesbool = fields.Boolean()                 #obligatory
+    diseases = fields.Char()                        #obligatory
+
+    #patientana = fields.Many2many(comodel_name="gestio_clinica_dental.patient", relation='anampatient')#restric to one
 
 
-class Medicalhistory (models.Model):
-    _name = 'gestio_clinica_dental.Medicalstory'
+class medicalhistory (models.Model):
+    _name = 'gestio_clinica_dental.medicalhistory'
     _description = 'Important regist medical'
+    #basic dates
+    datecreated = DateTime()
+    #forenkeys
+    patientmed =fields.Many2many(comodel_name='gestio_clinica_dental.patient', relation='medicalhistoryonepatient')#restric to one
+    textmed = fields.One2many(comodel_name='gestio_clinica_dental.textmedicalhistory', inverse_name='textwritemed')
 
-    pacientmed =fields.Many2many(comodel_name='gestio_clinica_dental.Patient', relation='medicalhistoryonepatient')#restric to one
-    textmed = fields.One2many(comodel_name='gestio_clinica_dental.Textmedicalhistory', inverse_name='textwritemed')
-
-
-class Textmedicalhistory (models.Model):
-    _name = 'gestio_clinica_dental.Textmedicalhistory'
+class textmedicalhistory (models.Model):
+    _name = 'gestio_clinica_dental.textmedicalhistory'
     _description = 'Text inside in the medical history and write from the doctor'
-
+    #basic dates
+    datewrited = fields.Datetime()
     insertiondate = fields.Date()
     shortdecriptionmedicalh = fields.Char()
     longdescriptionmedicalh = fields.Text()
-    textwritemed = fields.Many2one(comodel_name='gestio_clinica_dental.Medicalhistory')
+    #forenkeys
+    textwritemed = fields.Many2one(comodel_name='gestio_clinica_dental.medicalhistory')   
+
+
 
 
 # *************************************************************************************   
 # SCHEDULE DATES
 # ************************************************************************************* 
 
-class Principalschedule (models.Model):
-    _name = 'gestio_clinica_dental.Principalschedule'
-    _description = 'Organization pacients '
-
+class principalschedule (models.Model):
+    _name = 'gestio_clinica_dental.principalschedule'
+    _description = 'Organization patients '
+    #basic dates
     hours = fields.Datetime()
+    #forenkeys
+    principalschedulequo = fields.Many2many(comodel_name='gestio_clinica_dental.quotes', relation='quotesschedule')#quotes
+    principalscheduledoc = fields.Many2many(comodel_name='gestio_clinica_dental.doctor', relation='doctorschedule')#doctor
 
-    quotes = fields.Many2many(comodel_name='gestio_clinica_dental.Doctor', relation='doctorfrompatient')#quotes
-    doctorsinside = fields.Many2many(comodel_name='gestio_clinica_dental.Doctor', relation='doctorschedule')#doctor
-
-class Quotes (models.Model):
-    _name = 'gestio_clinica_dental.Quotes'
-    _description = 'Quotes and specification intervention '
-
-    time = fields.time()
-    doctor = fields.Many2many(comodel_name='gestio_clinica_dental.Doctor', relation='doctorfrompatient')#doctor
-    patient = fields.Many2one(comodel_name='gestio_clinica_dental.Patient', relation='quotesagendfrompatient')#patient 
-    
+class quotes (models.Model):
+    _name = 'gestio_clinica_dental.quotes'
+    _description = 'quotes and specification intervention '
+    #basic dates
+    idquotes = fields.Integer()
+    datetime = fields.Datetime()
+    #forenkeys
+    quotespat = fields.Many2one(comodel_name='gestio_clinica_dental.patient', relation='quotesagendfrompatient')#patient 
+    quotespri = fields.Many2many(comodel_name='gestio_clinica_dental.principalschedule', relation='quotesschedule')#quotes
 
 
 # *************************************************************************************   
 # MEDICAL DATES
 # *************************************************************************************
 
-class Treatment (models.Model):
-    _name = 'gestio_clinica_dental.Treatment'
-    _description = 'Treatment description'
-
+class treatment (models.Model):
+    _name = 'gestio_clinica_dental.treatment'
+    _description = 'treatment description'
+    #basic dates
     name = fields.Char()
     description = fields.Text()
-
+    #specificdates
     specialitytre = fields.Selection([('1','General'),('2','Surgery'),('3','Orthodontics'),('4','Endodontics'),('5','Stetic'),('6','Prosthodontist'),('7','Pediatric')])
     category = fields.Selection([('1','Primary'),('2','Secondary')])
-
     duration = fields.time()
     price = fields.Float()
+    #forenkeys
 
-    tratmenttra = fields.Many2many(comodel_name='gestio_clinica_dental.Treatment', relation='Treatment2category')
-    budgettre = fields.Many2many(comodel_name='gestio_clinica_dental.Budgettre', relation='TreatmentBudget')
-    Laboratorytra = fields.Many2many(comodel_name='gestio_clinica_dental.Laboratory', relation='Tratment <-> laboratory')
+    #tratmenttra = fields.Many2many(comodel_name='gestio_clinica_dental.treatment', relation='treatment2category')
+    tratmentcons = fields.Many2many(comodel_name='gestio_clinica_dental.consent', relation='treatmentconsent')
+    treatmentbud = fields.Many2many(comodel_name='gestio_clinica_dental.budget', relation='budgettreatment')
+    treatmentlab = fields.Many2many(comodel_name='gestio_clinica_dental.laboratory', relation='Tratmentlaboratory')
 
 
 
@@ -179,43 +199,45 @@ class Treatment (models.Model):
 # *************************************************************************************   
 # FINANCES DATES
 # ************************************************************************************* 
-class Budget (models.Model):
-    _name = 'gestio_clinica_dental.Budget'
+class budget (models.Model):
+    _name = 'gestio_clinica_dental.budget'
     _description = 'Intervention type'
-
+    #basic dates
     idbudget = fields.Integer()
     anotations = fields.Text()
     logoimg = fields.Image()
-
-    pacientbud = fields.One2many(comodel_name='gestio_clinica_dental.Pacient', inverse_name='budgetpac')#pacient
-    
-    assistantmake = fields.Many2many(comodel_name='gestio_clinica_dental.Medicalassistant', relation='assistatintervention')#assistant
     makedate = fields.Datetime()
-   
-    treatmentquo = fields.Many2many(comodel_name='gestio_clinica_dental.Treatment', relation='TreatmentBudget')
+    #specific dates
     duration    = fields.time()
-
-    pricetotal = fields.Float()#computed
     methotpay = fields.Selection([('1','full pay'), ('2','financed'), ('3','Make & pay'), ('4','Other')])
     discountmanual = fields.Float()
     totalprice = fields.Float()#2compute total and discount methot pay
-
-
+    #forenkeys
+    budgettre = fields.Many2many(comodel_name='gestio_clinica_dental.treatment', relation='budgettreatment')#patient
+    budgetpat = fields.One2many(comodel_name="gestio_clinica_dental.patient", inverse_name="patientbud")
+    assistantmake = fields.Many2many(comodel_name='gestio_clinica_dental.medicalassistant', relation='assistatintervention')#assistant
 
 # *************************************************************************************   
 # OTHERS DATES
 # ************************************************************************************* 
 
 
-class Laboratory (models.Model):
-    _name = 'gestio_clinica_dental.Textmedicalhistory'
+class laboratory (models.Model):
+    _name = 'gestio_clinica_dental.laboratory'
     _description = 'Text inside in the medical history and write from the doctor'
-
+    #basic dates
     name = fields.Char()
     description = fields.Char()
     phone1 = fields.Char()
     phone2 = fields.Char()
-
+    #specific dates
     speciaity = fields.Selection([('1','Stetic'),('2','Funcionality'),('3','All modalities')])
     timeproduction = fields.Integer()
-    treatmentlab = fields.Many2many(comodel_name='gestio_clinica_dental.Laboratorytra', relation='Tratment <-> laboratory')
+    #forenkeys
+    laboratorytre = fields.Many2many(comodel_name='gestio_clinica_dental.treatment', relation='Tratmentlaboratory')
+
+
+# *************************************************************************************   
+# ADITIONAL TABLES FROM MANY2MANY
+# ************************************************************************************* 
+
